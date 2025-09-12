6a9eda4ce3a908a4146d80a08ada919ef927ec11
AnnaMatilda-IT <annaeliseeva86@mail.ru>
2025-09-12 23:14:27 +0500

    done

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,20 +16,40 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+       // Создаем машины отдельно
+       Car car1 = new Car("Toyota", 2020);
+       Car car2 = new Car("Honda", 2021);
+       Car car3 = new Car("BMW", 2022);
+       Car car4 = new Car("Mercedes", 2023);
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+       // Создаем пользователей с машинами
+       userService.add(new User("User1", "Lastname1", "user1@mail.ru", car1));
+       userService.add(new User("User2", "Lastname2", "user2@mail.ru", car2));
+       userService.add(new User("User3", "Lastname3", "user3@mail.ru", car3));
+       userService.add(new User("User4", "Lastname4", "user4@mail.ru", car4));
 
-      context.close();
+       // Выводим всех пользователей
+       List<User> users = userService.listUsers();
+       for (User user : users) {
+           System.out.println("Id = " + user.getId());
+           System.out.println("First Name = " + user.getFirstName());
+           System.out.println("Last Name = " + user.getLastName());
+           System.out.println("Email = " + user.getEmail());
+           if (user.getCar() != null) {
+               System.out.println("Car = " + user.getCar().getModel() + " " + user.getCar().getSeries());
+           }
+           System.out.println();
+       }
+
+       // Тестируем поиск по машине
+       System.out.println("=== Поиск владельца BMW 2022 ===");
+       User bmwOwner = userService.findUserByCar("BMW", 2022);
+       if (bmwOwner != null) {
+           System.out.println("Владелец BMW: " + bmwOwner.getFirstName() + " " + bmwOwner.getLastName());
+       } else {
+           System.out.println("Владелец не найден");
+       }
+
+       context.close();
    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,6 +1,7 @@
 package hiber.dao;
 
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -16,14 +17,37 @@ public class UserDaoImp implements UserDao {
 
    @Override
    public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
+
+       Session session = sessionFactory.getCurrentSession();
+       // Если у пользователя есть машина, сохраняем ее сначала
+       if (user.getCar() != null) {
+           session.save(user.getCar());
+       }
+       // Затем сохраняем пользователя
+       session.save(user);
    }
 
    @Override
-   @SuppressWarnings("unchecked")
    public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+       TypedQuery<User> query =
+               sessionFactory.getCurrentSession().createQuery("from User", User.class);
       return query.getResultList();
    }
 
+    @Override
+    public User findByCarModelAndSeries(String model, int series) {
+        TypedQuery<User> query =
+                sessionFactory.getCurrentSession().createQuery(
+                "SELECT u FROM User u JOIN u.car c WHERE c.model = :model AND c.series = :series",
+                User.class
+        );
+        query.setParameter("model", model);
+        query.setParameter("series", series);
+
+        try {
+            return query.getSingleResult();
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
